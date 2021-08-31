663ee11a90d520ba90016469fb15f1b1d051744f
Christian Afreh <55987185+ChristianAfreh@users.noreply.github.com>
2021-08-31 14:55:25 +0000

    initial commit

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/mylib', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const paymentSchema = new mongoose.Schema({
+    id: String,
+    itemId: String,
+    paid: Boolean
+});
+
+const Payment = mongoose.model('Payment', paymentSchema);
+
+module.exports = {
+    Payment
+};
